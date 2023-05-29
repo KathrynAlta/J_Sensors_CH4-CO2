fd1501004c93cb6242a65d449bef975efdf202ba
Katie Ganon <54601749+KathrynAlta@users.noreply.github.com>
2023-05-29 16:58:40 -0400

    calibration

--- 02_Slope_Calculation.R
+++ 02_Slope_Calculation.R
@@ -38,13 +38,16 @@
         tibble(path = ., round = "Round_1") %>%   #make into tbl of path and sensor name 
         mutate(data = lapply(path, read_csv)) %>%  
         unnest(data) -> raw_data
+      # mt Pleasant is throwing this off, probably from when I renamed the csv files to include SE 
       
       raw_data$ID <- substring(raw_data$path, 3, nchar(raw_data$path)-4)  # Make an ID column based on path and file name
       raw_data$Sensor <- substring(raw_data$ID, nchar(raw_data$ID)-1, nchar(raw_data$ID))  # Make a sensor column based on the path and file name 
     
     # Times on an Off 
-    
+    time_window <- read_xlsx("~/J_Sensors_CH4-CO2/Input_Files/JSensor_Start_Stop_Times.xlsx")
+      
     # Calibration coefficients 
+    calib_coef <- read_xlsx("~/J_Sensors_CH4-CO2/Output_File/model_coef_230511.xlsx")
     
 # 2. Trim files by start and end time 
       # Seperate out by ID into a list of dfs rather than one big df 
